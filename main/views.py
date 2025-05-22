b2d8b325109ec0b5173860e05010a2a8c9e93646
RomanGroz <romangrozh@gmail.com>
2025-05-22 19:55:43 +0300

    first1

---
+++ main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
+def index(request):
+    return render(request, 'main/index.html') 
+
+def about(request):
+    return HttpResponse('About page') 
