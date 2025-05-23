5ce5f14cce5590332e28d5bd79a6d66fd76899ef
RomanGroz <romangrozh@gmail.com>
2025-05-23 15:45:31 +0300

    first2 main app is ready, index and about complited

--- main/views.py
+++ main/views.py
@@ -2,7 +2,26 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
 def index(request):
-    return render(request, 'main/index.html') 
+
+    context = {
+
+        'title': 'HOME - Главная',
+        'content': 'Магазин мебели HOME',
+
+
+    }
+
+    return render(request, 'main/index.html', context) 
 
 def about(request):
-    return HttpResponse('About page') 
+
+    context = {
+
+        'title': 'HOME - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Текст о том почему этот магазин такой хороший и товар такой крутой',
+
+
+    }
+
+    return render(request, 'main/about.html', context) 
