743159fc4b08c7b5203baf8516b30e37cf264b68
RomanGroz <romangrozh@gmail.com>
2025-05-25 00:44:52 +0300

    first12 bd postgresql and full text search well done

--- app/settings.py
+++ app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 
     'main',
     'goods',
@@ -81,8 +82,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'home',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- goods/utils.py
+++ goods/utils.py
@@ -1,6 +1,5 @@
-import keyword
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.db.models import Q
-
 from goods.models import Products
 
 
@@ -9,12 +8,17 @@ def q_search(queary):
     if queary.isdigit() and len(queary) <= 5:
         return Products.objects.filter(id=int(queary))
     
-    keyword = [word for word in queary.split() if len(word) > 2]
+    vector = SearchVector("name", "description")
+    query = SearchQuery(queary) 
 
-    q_objects = Q()
+    return Products.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by("-rank")
 
-    for token in keyword:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
 
-    return Products.objects.filter(q_objects)
+#    keyword = [word for word in queary.split() if len(word) > 2]
+
+#    q_objects = Q()
+
+#        q_objects |= Q(description__icontains=token)
+#       q_objects |= Q(name__icontains=token)
+
+#    return Products.objects.filter(q_objects)
