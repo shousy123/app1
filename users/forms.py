dc762d58df4ae6c76907d1a03bf5f6ad42fdcc05
RomanGroz <romangrozh@gmail.com>
2025-05-25 20:16:20 +0300

    first15 login form add

---
+++ users/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+from users.models import User
+
+class UserLoginForm(AuthenticationForm):
+
+    username = forms.CharField()
+    password = forms.CharField()
+
+
+    class Meta:
+        model = User
+        field = ['username', 'password']
