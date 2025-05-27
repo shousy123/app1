01b9b38946f219eee2b39fe3d68667e313151e03
RomanGroz <romangrozh@gmail.com>
2025-05-27 17:39:16 +0300

    first24 admin panel setup

--- goods/models.py
+++ goods/models.py
@@ -1,5 +1,6 @@
 from tabnanny import verbose
 from django.db import models
+from django.urls import reverse
 
 class Categories(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name='Название')
@@ -33,6 +34,10 @@ class Products(models.Model):
     def __str__(self):
         return f'{self.name} Количество - {self.quantity}' 
     
+    def get_absolute_url(self):
+        return reverse("catalog:product", kwargs={"product_slug": self.slug})
+    
+
     def display_id(self):
         return f'{self.id:05}'
     
