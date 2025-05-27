a135920ee8403ecb60825e534934a4cba0bfd2bd
RomanGroz <romangrozh@gmail.com>
2025-05-27 13:50:24 +0300

    first22 ajax all done

--- carts/views.py
+++ carts/views.py
@@ -7,7 +7,6 @@ from carts.utils import get_user_carts
 from goods.models import Products
 
 def cart_add(request):
-
     product_id = request.POST.get("product_id")
     product = Products.objects.get(id=product_id)
 
@@ -33,11 +32,47 @@ def cart_add(request):
 
     return JsonResponse(response_data)
 
-def cart_change(request, productt_slug):
-    ...
+def cart_change(request):
+    cart_id = request.POST.get("cart_id")
+    quantity = request.POST.get("quantity")
 
-def cart_remove(request,  cart_id):
     cart = Cart.objects.get(id=cart_id)
+
+    cart.quantity = quantity
+    cart.save()
+    updated_quantity = cart.quantity
+
+    cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": cart}, request=request
+    )
+
+    response_data = {
+        "message": "Количество изменено",
+        "cart_items_html": cart_items_html,
+        "quantity": updated_quantity,
+    }
+
+    return JsonResponse(response_data)
+
+
+def cart_remove(request):
+    cart_id = request.POST.get("cart_id")
+
+    cart = Cart.objects.get(id=cart_id)
+    quantity = cart.quantity
     cart.delete()
-    
-    return redirect(request.META['HTTP_REFERER'])
+
+    user_cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request
+    )
+
+    response_data = {
+        "message": "Товар удален",
+        "cart_items_html": cart_items_html,
+        "quantity_deleted": quantity,
+    }
+
+    return JsonResponse(response_data)
+
