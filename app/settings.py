aa17f841fb68d5d6177cf689aa52979d8d0906f1
RomanGroz <romangrozh@gmail.com>
2025-05-30 05:15:43 +0300

    second2 optimazed

--- app/settings.py
+++ app/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.postgres',
 
+    'markdownfield',
+
     'main',
     'goods',
     'users',
--- goods/models.py
+++ goods/models.py
@@ -1,6 +1,7 @@
-from tabnanny import verbose
 from django.db import models
 from django.urls import reverse
+from markdownfield.models import MarkdownField, RenderedMarkdownField
+from markdownfield.validators import VALIDATOR_STANDARD
 
 class Categories(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name='Название')
@@ -18,11 +19,20 @@ class Categories(models.Model):
 class Products(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name='Название')
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
-    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+
+    description = MarkdownField(
+        rendered_field='description_rendered',
+        validator=VALIDATOR_STANDARD,
+        blank=True,
+        null=True,
+        verbose_name='Описание'
+    )
+    description_rendered = RenderedMarkdownField(null=True)
+
     image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Изображение')
-    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена')   
+    price = models.DecimalField(default=0.00, max_digits=9, decimal_places=2, verbose_name='Цена')   
     discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Скидка в %')
-    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')   
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество на складе')   
     category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
 
     class Meta:
